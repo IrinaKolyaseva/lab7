bf625189fc2d290de83c043dab34d10aaea05cfd
Ирина_Колясева <irinakolyaseva95@gmail.com>
2021-12-12 12:27:39 +0300

    Добавьте файлы проекта.

---
+++ Lb7_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стророны 1 треугольника");
+            double ao = Convert.ToDouble(Console.ReadLine());
+            double ad = Convert.ToDouble(Console.ReadLine());
+            double at = Convert.ToDouble(Console.ReadLine());
+            double so = Geron(ao, ad, at);
+
+            Console.WriteLine("Введите стророны 2 треугольника");
+            double bo = Convert.ToDouble(Console.ReadLine());
+            double bd = Convert.ToDouble(Console.ReadLine());
+            double bt = Convert.ToDouble(Console.ReadLine());
+            double sd = Geron(bo, bd, bt);
+
+
+            if (so > sd)
+            {
+                Console.WriteLine("S первого треугольника больше = {0} \nS второго треугольника меньше = {1:N2}", so, sd);
+            }
+            else if (so < sd)
+            {
+                Console.WriteLine("S первого треугольника меньше = {0} \n S второго треугольника больше  = {1:N2}", so, sd);
+            }
+            else if (so == sd && so != 0)
+            {
+                Console.WriteLine("S первого и второго треугольника равна = {0}= {1:N2}", so, sd);
+            }
+            Console.ReadKey();
+        }
+        static double Geron (double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            VScub(a);
+            Console.ReadKey();
+        }
+        static void VScub(double a)
+        {
+            double V = Math.Pow(a, 3);
+            double S = (6 * Math.Pow(a, 2));
+            Console.WriteLine("L ребра = {0} \n V = {1:N2} \n S пов-ти = {2:N2} ", a, V, S);
+        }
+    }
+}
